dd3baa5d624bbf599c0e395dab1db1df17f138bd
Sergio Ezequiel Garro <85518970+EZzero7@users.noreply.github.com>
2022-11-08 12:55:24 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,220 @@
+#Python
+import json
+from typing import Optional, List
+from enum import Enum
+from uuid import UUID
+from datetime import date
+from datetime import datetime
+
+#Pydantic
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic import EmailStr
+
+#FastAPI
+from fastapi import FastAPI
+from fastapi import status
+from fastapi import HTTPException
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
+
+
+app = FastAPI()
+
+#Models
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=50
+    )
+
+    
+
+class User(UserBase):
+    first_name : str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Sergio"
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="garro"
+    )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115,
+        example="25"
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+class UserRegister(User):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=50
+    )
+
+class Tweet(BaseModel):
+    tweet_id: UUID = Field(...),
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
+
+
+
+
+
+## Users
+
+@app.post(
+    path="/signup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags=["Users"]
+    )
+def signup(user: UserRegister = Body(...)):
+    with open("users.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        user_dict = user.dict()
+        user_dict["user_id"] = str(user_dict["user_id"])
+        user_dict["birth_date"] = str(user_dict["birth_date"])
+        results.append(user_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+
+
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a User",
+    tags=["Users"]
+    )
+def login(person : User = Body(...)):
+    return person
+
+@app.get(
+    path="/users",
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary="Show all Users",
+    tags=["Users"]
+    )
+def show_all_users(person : User = Body(...)):
+    with open("users.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
+
+@app.get(
+    path="/users/{user_id}",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Show a Users",
+    tags=["Users"]
+    )
+def show_a_user(person : User = Body(...)):
+    return person
+
+@app.delete(
+    path="/users/{user_id}/delete",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a Users",
+    tags=["Users"]
+    )
+def delete_a_user(person : User = Body(...)):
+    return person
+
+@app.put(
+    path="/users/{user_id}/update",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="update a Users",
+    tags=["Users"]
+    )
+def update_a_user(person : User = Body(...)):
+    return person
+
+
+## Tweets
+
+@app.get(
+    path="/",
+    response_model=List[Tweet],
+    status_code=status.HTTP_200_OK,
+    summary="Show all tweets",
+    tags=["Home"]
+    )
+def home():
+    with open("tweets.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
+
+@app.post(
+    path="/post",
+    response_model=Tweet,
+    status_code=status.HTTP_201_CREATED,
+    summary="Post a tweet",
+    tags=["Users"]
+    )
+def post(tweet: Tweet = Body(...)):
+    with open("users.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
+
+
+@app.post(
+    path="/tweets/{tweet_id}",
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary="Show a tweet",
+    tags=["Users"]
+    )
+def show_a_tweet(person : User = Body(...)):
+    return person
+
+@app.delete(
+    path="/tweets/{tweet_id}/delete",
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary="Delete a tweet",
+    tags=["Users"]
+    )
+def delete_a_tweet(person : User = Body(...)):
+    return person
+
+@app.put(
+    path="/tweets/{tweet_id}/update",
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary="update a tweet",
+    tags=["Users"]
+    )
+def update_a_tweet(person : User = Body(...)):
+    return person
